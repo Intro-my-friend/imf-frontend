bf2fbceeea81151d49ec28dd7b1ef64511994d90
nohgijin <ynda11111@naver.com>
2025-07-11 15:55:09 +0900

    feat: 로그인 페이지

---
+++ app/login/page.tsx
@@ -0,0 +1,16 @@
+import $ from './page.module.css'
+
+export default function Login(){
+    return(
+        <div className={$.login}>
+            <div className={$['continue-wrapper']}>
+                <div className={$.continue}>
+                    네이버로 계속하기
+                </div>
+                <div className={$.continue}>
+                    카카오로 계속하기
+                </div>
+            </div>
+        </div>
+    )
+}
