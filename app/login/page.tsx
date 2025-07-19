29f2c0f2386091ca6e0816100976e34de78fba9b
nohgijin <ynda11111@naver.com>
2025-07-19 18:36:14 +0900

    fix: uri 수정

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -5,10 +5,13 @@ import { signIn } from "next-auth/react";
 import $ from './page.module.scss'
 
 export default function Login(){
+    const handleSocialLogin = (provider: 'kakao' | 'naver') => {
+        window.location.href = `http://15.164.39.230:8000/api/v0/auth/${provider}/login`;
+    };
     return(
         <div className={$.login}>
             <div className={$['continue-wrapper']}>
-                <button className={$.continue} onClick={() => signIn("kakao", { redirect: true, callbackUrl: "/" })}>
+                <button className={$.continue} onClick={() => handleSocialLogin('kakao')}>
                     네이버로 계속하기
                 </button>
                 <button className={$.continue} onClick={() => signIn("naver", { redirect: true, callbackUrl: "/" })}>
