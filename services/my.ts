f416461f46ea55d239f7e397be2e9cc605590e1d
양동진 <dong3831@hanmail.net>
2025-08-08 17:02:14 +0900

    add mypage

---
+++ services/my.ts
@@ -0,0 +1,55 @@
+export async function fetchUserInfo(
+  token: string,
+) {
+  const response = await fetch(
+    `http://15.164.39.230:8000/api/v0/users`, 
+    {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${token}`,
+      },
+    }
+  );
+
+  if (!response.ok) throw new Error("유저 정보 요청 실패");
+  return response.json();
+}
+
+export async function patchPurpose(
+  introduction: boolean,
+  token: string
+) {
+  const response =  await fetch(
+    `http://15.164.39.230:8000/api/v0/users/matching-settings`,
+    {
+      method: "PATCH",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${token}`,
+      },
+      body: JSON.stringify({ introduction: introduction }),
+    }
+);
+
+  if (!response.ok) throw new Error("모드 변경 실패");
+  return response.json();
+}
+
+export async function getUserImages(
+  token: string,
+) {
+  const response = await fetch(
+    `http://15.164.39.230:8000/api/v0/users/images`, 
+    {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${token}`,
+      },
+    }
+  );
+
+  if (!response.ok) throw new Error("유저 이미지 요청 실패");
+  return response.json();
+}
